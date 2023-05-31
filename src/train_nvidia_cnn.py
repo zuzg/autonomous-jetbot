3d6d235084749c3448d69a0fd4475049fb81395c
Zuzanna Gawrysiak <zuzanna.gawrysiak@intel.com>
2023-05-31 15:00:57 +0200

    Add draft nvidia cnn pipeline

---
+++ src/train_nvidia_cnn.py
@@ -0,0 +1,18 @@
+def train(net, trainloader, valloader, optimizer, criterion, epochs, device="cuda"):
+    net.to(device)
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
